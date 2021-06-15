2dd2aadd535f8aa258ddda8a37473e80bcc564ea
Jeremy Fox <jeremy.fox@cps.scot>
2021-06-15 12:45:09 +0100

    Adds tests for GetSwimmingSpotById action

--- SwimSpot.Api.Tests/Models/SwimmingSpotControllerTests.cs
+++ SwimSpot.Api.Tests/Models/SwimmingSpotControllerTests.cs
@@ -3,10 +3,7 @@ using Moq;
 using SwimSpot.Api.Controllers;
 using SwimSpot.Api.Models;
 using SwimSpot.Shared;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Xunit;
 
 
@@ -23,7 +20,7 @@ namespace SwimSpot.Api.Tests.Models
             _swimmingSpotController = new SwimmingSpotController(_mockRepo.Object);
             
         }
-
+     
         [Fact]
         public void GetAllSwimmingSpots_ReturnsOkObjectResult()
         {
@@ -33,17 +30,60 @@ namespace SwimSpot.Api.Tests.Models
         [Fact]
         public void GetAllSwimmingSpots_ActionExecutes_ReturnsExactNumberOfSwimmingSpots()
         {
+            //arrange
             _mockRepo.Setup(repo => repo.GetAllSwimmingSpots())
                 .Returns(new List<SwimmingSpot>() { new SwimmingSpot(), new SwimmingSpot() });
-
+            //act
             var result = _swimmingSpotController.GetAllSwimmingSpots();
-
+            //assert
             var okObjectResult = Assert.IsType<OkObjectResult>(result);
 
             var swimmingSpots = Assert.IsType<List<SwimmingSpot>>(okObjectResult.Value);
+
             Assert.Equal(2, swimmingSpots.Count);
         }
 
+        [Fact]
+        public void GetSwimmingSpotById_UnknownIdPassed_ReturnsNotFoundResult()
+        {
+            // arrange
+            var fakeId = 2;
+            
+            // act
+            var notFoundResult = _swimmingSpotController.GetSwimmingSpotById(fakeId);
+            
+            // assert
+            Assert.IsType<NotFoundResult>(notFoundResult);
+        }
 
+        [Fact]
+        public void GetSwimmingSpotById_ExistingIdPassed_ReturnsOkResult()
+        {
+            
+            var testId = 1;
+            // arrange
+            _mockRepo.Setup(repo => repo.GetSwimmingSpotById(testId))
+                .Returns(new SwimmingSpot() { SwimmingSpotId = 1, Latitude = 55.645521772338500M, Longitude = -2.906058530106536M, Comments = new List<SwimmingSpotComment>() });
+            // act
+            var okResult = _swimmingSpotController.GetSwimmingSpotById(testId);
+            // assert
+            Assert.IsType<OkObjectResult>(okResult);
+
+        }
+
+        [Fact]
+        public void GetSwimmingSpotById_ExistingIdPassed_ReturnsRightItem()
+        {
+            // arrange
+            var testId = 1;
+            _mockRepo.Setup(repo => repo.GetSwimmingSpotById(testId))
+                .Returns(new SwimmingSpot() { SwimmingSpotId = 1, Latitude = 55.645521772338500M, Longitude = -2.906058530106536M, Comments = new List<SwimmingSpotComment>()});
+            // act
+            var result = _swimmingSpotController.GetSwimmingSpotById(testId);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            // assert
+            Assert.IsType<SwimmingSpot>(okResult.Value);
+            Assert.Equal(testId, (okResult.Value as SwimmingSpot).SwimmingSpotId);
+        }
     }
 }
