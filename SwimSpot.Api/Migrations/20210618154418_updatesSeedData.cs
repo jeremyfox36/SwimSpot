0e5af302305b5012235426d015fb896d23a0455b
Jeremy Fox <jeremy.fox@cps.scot>
2021-06-19 21:36:52 +0100

    Adds new seed data

---
+++ SwimSpot.Api/Migrations/20210618154418_updatesSeedData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SwimSpot.Api.Migrations
+{
+    public partial class updatesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SwimmingSpots",
+                keyColumn: "SwimmingSpotId",
+                keyValue: 1,
+                column: "SwimmingSpotName",
+                value: "Stantling Craigs");
+
+            migrationBuilder.UpdateData(
+                table: "SwimmingSpots",
+                keyColumn: "SwimmingSpotId",
+                keyValue: 2,
+                column: "SwimmingSpotName",
+                value: "Gladhouse Reservoir");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SwimmingSpots",
+                keyColumn: "SwimmingSpotId",
+                keyValue: 1,
+                column: "SwimmingSpotName",
+                value: "Gladhouse Reservoir");
+
+            migrationBuilder.UpdateData(
+                table: "SwimmingSpots",
+                keyColumn: "SwimmingSpotId",
+                keyValue: 2,
+                column: "SwimmingSpotName",
+                value: "Stantling Craigs");
+        }
+    }
+}
--- SwimSpot.Api/Models/AppDbContext.cs
+++ SwimSpot.Api/Models/AppDbContext.cs
@@ -18,14 +18,15 @@ namespace SwimSpot.Api.Models
         public DbSet<SwimmingSpot> SwimmingSpots { get; set; }
         public DbSet<SwimmingSpotComment> SwimmingSpotComments { get; set; }
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<SwimmingSpot>().HasData(new SwimmingSpot { SwimmingSpotId = 1, SwimmingSpotName = "Gladhouse Reservoir", Latitude = 55.6455217723385M, Longitude = -2.9060585301065363M });
+            modelBuilder.Entity<SwimmingSpot>().HasData(new SwimmingSpot { SwimmingSpotId = 1, SwimmingSpotName = "Stantling Craigs", Latitude = 55.6455217723385M, Longitude = -2.9060585301065363M });
             modelBuilder.Entity<SwimmingSpotComment>().HasData(new SwimmingSpotComment { SwimmingSpotCommentId = 1, SwimmingSpotId = 1, CommentDate = new DateTime(2021, 06, 08), SwimDate = new DateTime(2021, 06, 06), WaterTemp = 20, Content = "Swam along west shore with Doug", UserId = 1, UserName = "Jem" });
 
-            modelBuilder.Entity<SwimmingSpot>().HasData(new SwimmingSpot { SwimmingSpotId = 2, SwimmingSpotName = "Stantling Craigs", Latitude = 55.77398808982048M, Longitude = -3.1221541644140895M });
+            modelBuilder.Entity<SwimmingSpot>().HasData(new SwimmingSpot { SwimmingSpotId = 2, SwimmingSpotName = "Gladhouse Reservoir", Latitude = 55.77398808982048M, Longitude = -3.1221541644140895M });
             modelBuilder.Entity<SwimmingSpotComment>().HasData(new SwimmingSpotComment { SwimmingSpotCommentId = 2, SwimmingSpotId = 2, CommentDate = new DateTime(2021, 06, 08), SwimDate = new DateTime(2021, 06, 06), WaterTemp = 20, Content = "Swam around the island", UserId = 1, UserName = "Jem" });
         }
     }
