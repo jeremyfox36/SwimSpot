ae91a709b3b5afabc9de30cd770ac0b7667cb6f7
Jeremy Fox <jeremy.fox@cps.scot>
2021-06-22 12:21:31 +0100

    Adds temporary test controller

---
+++ SwimSpot.Api/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwimSpot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Got the test route");
+        }
+    }
+}
